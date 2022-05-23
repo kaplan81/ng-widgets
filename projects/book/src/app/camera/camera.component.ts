0f545fcd112d4671414a3ba00f476fae7b060cca
“kaplan81” <gesteira.pro@gmail.com>
2022-05-23 15:53:17 +0200

    camera component

--- projects/book/src/app/camera/camera.component.ts
+++ projects/book/src/app/camera/camera.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'bkc-camera',
   templateUrl: './camera.component.html',
-  styleUrls: ['./camera.component.scss']
+  styleUrls: ['./camera.component.scss'],
 })
-export class CameraComponent implements OnInit {
+export class CameraComponent implements AfterViewInit {
+  @ViewChild('video') video: ElementRef | null = null;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    if (this.video !== null) {
+      const videoEl: HTMLVideoElement = this.video.nativeElement;
+      navigator.mediaDevices
+        .getUserMedia({
+          video: { facingMode: 'user' },
+        })
+        .then((stream: MediaStream) => (videoEl.srcObject = stream));
+    }
   }
-
 }
