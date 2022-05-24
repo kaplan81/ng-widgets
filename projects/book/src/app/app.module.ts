1e208ea59af1a82e3633446ba7f7e8b56ca0186f
“kaplan81” <gesteira.pro@gmail.com>
2022-05-24 10:46:51 +0200

    create angular element

--- projects/book/src/app/app.module.ts
+++ projects/book/src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement, NgElementConstructor } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CameraComponent } from './camera/camera.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CameraComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CameraComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    const cameraComponent: NgElementConstructor<CameraComponent> =
+      createCustomElement(CameraComponent, { injector });
+    customElements.define('bkc-camera', cameraComponent);
+  }
+}
